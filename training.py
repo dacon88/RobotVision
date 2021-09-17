e51c155d55c120ecec5226e86000d6c1d01d3a9c
Davide Congiu <davidecongiu@hotmail.it>
2021-09-17 15:15:46 +0200

    training as script which runs model training

--- training.py
+++ training.py
@@ -2,3 +2,14 @@
 training handles the modified AlexNet NN training process
 """
 
+from alexnet_finetune import AlexnetFinetune
+
+
+def main():
+    alexnet_finetune = AlexnetFinetune()
+
+    alexnet_finetune.train_model()
+
+
+if __name__ == "__main__":
+    main()
