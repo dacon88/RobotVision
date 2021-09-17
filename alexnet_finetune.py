ee143343954cba91a73b2e6e244c04d816883f78
Davide Congiu <davidecongiu@hotmail.it>
2021-09-17 14:49:30 +0200

    change params into attributes

--- alexnet_finetune.py
+++ alexnet_finetune.py
@@ -10,11 +10,12 @@ import copy
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import numpy as np
 import torchvision
-from torchvision import datasets, models, transforms
+from torchvision import datasets, transforms
+
 
 class AlexnetFinetune:
+
     def __init__(self):
         # Top level data directory. Here we assume the format of the directory conforms
         #   to the ImageFolder structure
@@ -94,35 +95,35 @@ class AlexnetFinetune:
         # Setup the loss fxn
         self.criterion = nn.CrossEntropyLoss()
 
-    def train_model(device, model, dataloaders, criterion, optimizer, num_epochs=25, is_inception=False):
+    def train_model(self, is_inception=False):
         since = time.time()
 
         val_acc_history = []
 
-        best_model_wts = copy.deepcopy(model.state_dict())
+        best_model_wts = copy.deepcopy(self.model.state_dict())
         best_acc = 0.0
 
-        for epoch in range(num_epochs):
-            print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        for epoch in range(self.num_epochs):
+            print('Epoch {}/{}'.format(epoch, self.num_epochs - 1))
             print('-' * 10)
 
             # Each epoch has a training and validation phase
             for phase in ['train', 'val']:
                 if phase == 'train':
-                    model.train()  # Set model to training mode
+                    self.model.train()  # Set model to training mode
                 else:
-                    model.eval()   # Set model to evaluate mode
+                    self.model.eval()   # Set model to evaluate mode
 
                 running_loss = 0.0
                 running_corrects = 0
 
                 # Iterate over data.
-                for inputs, labels in dataloaders[phase]:
-                    inputs = inputs.to(device)
-                    labels = labels.to(device)
+                for inputs, labels in self.dataloaders[phase]:
+                    inputs = inputs.to(self.device)
+                    labels = labels.to(self.device)
 
                     # zero the parameter gradients
-                    optimizer.zero_grad()
+                    self.optimizer.zero_grad()
 
                     # forward
                     # track history if only in train
@@ -133,34 +134,34 @@ class AlexnetFinetune:
                         #   but in testing we only consider the final output.
                         if is_inception and phase == 'train':
                             # From https://discuss.pytorch.org/t/how-to-optimize-inception-model-with-auxiliary-classifiers/7958
-                            outputs, aux_outputs = model(inputs)
-                            loss1 = criterion(outputs, labels)
-                            loss2 = criterion(aux_outputs, labels)
+                            outputs, aux_outputs = self.model(inputs)
+                            loss1 = self.criterion(outputs, labels)
+                            loss2 = self.criterion(aux_outputs, labels)
                             loss = loss1 + 0.4*loss2
                         else:
-                            outputs = model(inputs)
-                            loss = criterion(outputs, labels)
+                            outputs = self.model(inputs)
+                            loss = self.criterion(outputs, labels)
 
                         _, preds = torch.max(outputs, 1)
 
                         # backward + optimize only if in training phase
                         if phase == 'train':
                             loss.backward()
-                            optimizer.step()
+                            self.optimizer.step()
 
                     # statistics
                     running_loss += loss.item() * inputs.size(0)
                     running_corrects += torch.sum(preds == labels.data)
 
-                epoch_loss = running_loss / len(dataloaders[phase].dataset)
-                epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+                epoch_loss = running_loss / len(self.dataloaders[phase].dataset)
+                epoch_acc = running_corrects.double() / len(self.dataloaders[phase].dataset)
 
                 print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
 
                 # deep copy the model
                 if phase == 'val' and epoch_acc > best_acc:
                     best_acc = epoch_acc
-                    best_model_wts = copy.deepcopy(model.state_dict())
+                    best_model_wts = copy.deepcopy(self.model.state_dict())
                 if phase == 'val':
                     val_acc_history.append(epoch_acc)
 
@@ -171,11 +172,5 @@ class AlexnetFinetune:
         print('Best val Acc: {:4f}'.format(best_acc))
 
         # load best model weights
-        model.load_state_dict(best_model_wts)
-        return model, val_acc_history
-
-
-    def set_parameter_requires_grad(model, feature_extracting):
-        if feature_extracting:
-            for param in model.parameters():
-                param.requires_grad = False
+        self.model.load_state_dict(best_model_wts)
+        return val_acc_history
