7cdb8d534e78757a5b616f6ee422106ac84120c1
Davide Congiu <davidecongiu@hotmail.it>
2021-09-19 08:42:20 +0200

    refactor inference py

--- inference.py
+++ inference.py
@@ -10,48 +10,47 @@ import cv2
 
 def main():
 
-    #Load model
+    print("Load model")
     nn = AlexnetFinetune()
     nn.model.load_state_dict(torch.load("state_dict_model.pt", map_location=torch.device('cpu')))
     nn.model.eval()
 
-    # pixel coord for cropping the webcam img
-    x1 = 384
-    y1 = 104
-
-    x2 = 896
-    y2 = 616
-
+    # get img data from webcam
     cv2.namedWindow("preview")
     vc = cv2.VideoCapture(0)
-
     if vc.isOpened():  # try to get the first frame
         rval, frame = vc.read()
     else:
         rval = False
 
+    print("inference")
+
+    # Predict on the fly images
     while rval:
-        #TODO: fix this variable horible thing
+
         img_bgr = frame
-        #TODO: maybe crop on purpose
-        #img_rgb = frame[y1:y2, x1:x2]
 
-        #cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-        dim = (160, 160)
-        img_bgr = cv2.resize(img_bgr, dim, interpolation=cv2.INTER_AREA)
-        cv2.imwrite("cv2.jpg", img_bgr)
-        im_PIL = Image.fromarray(img_bgr)
-        R, G, B = im_PIL.split()
+        # process camera img
+        # resize
+        new_resize_dim = (160, 160)
+        img_bgr = cv2.resize(img_bgr, new_resize_dim, interpolation=cv2.INTER_AREA)
 
+        # write img to disk for debugging
+        # cv2.imwrite("cv2.jpg", img_bgr)
+
+        # Convert CV2 to PIL and from RGB to BRG. Note: could not manage to have cv2.cvtColor working
+        im_pil = Image.fromarray(img_bgr)
+        R, G, B = im_pil.split()
         im_rgb = Image.merge("RGB", (B, G, R))
-        # im.show()
-        im1 = im_rgb.save("input_rgb.jpg")
+
+        # write img to disk for debugging
+        img_to_disk = im_rgb.save("PIL_input_rgb.jpg")
 
         x = nn.predict_image(im_rgb)
-        print(x)
+        # print(x)
         response = x
 
-        # cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+        # Render to screen
         cv2.putText(frame, response, (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), lineType=cv2.LINE_AA)
         cv2.imshow("preview", frame)
         rval, frame = vc.read()
@@ -59,8 +58,6 @@ def main():
         if key == 27:  # exit on ESC
             break
     cv2.destroyWindow("preview")
-
-    print("inference")
 
 
 if __name__ == "__main__":
